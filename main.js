a874bef45c27fe857912d5e807519d243fa4184d
Giuseppeworkjujujj <you@example.com>
2023-01-15 22:09:46 +0100

    second step

---
+++ main.js
@@ -0,0 +1,20 @@
+Vue.config.devtools = true;
+Vue.config.debug = true;
+
+const app = new Vue (
+    {
+        el: "#root",
+        data:{
+            cane: ["dmkfns", "jfnkjdnh", "ksnfln"],
+            object: {
+                firstName: 'John',
+                lastName: 'Doe',
+                age: 30
+              }
+        },
+        methods: {
+       
+            }
+        }
+    
+)
