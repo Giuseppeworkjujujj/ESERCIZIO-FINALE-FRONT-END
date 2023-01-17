3c1ed4b446958e810659501b938a3c44fae6971e
Giuseppeworkjujujj <you@example.com>
2023-01-17 18:24:56 +0100

    ciclo for

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ Vue.config.debug = true;
 const app = new Vue (
     {
         el: "#root",
-        data:{
-            cane: ["dmkfns", "jfnkjdnh", "ksnfln"],
-            object: {
-                firstName: 'John',
-                lastName: 'Doe',
-                age: 30
-              }
-        },
+        data: {
+            scritte: [
+            {
+                name: "flmsdfjsrifjironflr",
+            },
+            {
+                name: "flmsdfjsrifjironflr",
+            },
+        ]},
         methods: {
-       
-            }
-        }
-    
+            
+        },
+
+    }
+
 )
